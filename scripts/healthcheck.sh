10c3944108aaaf0041f407f5fe18747fa45b70ff
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2026-09-21 12:09:55 +1000

    Fix healthcheck to use wget

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -4,8 +4,8 @@ FILE=/opt/adguardhome/conf/AdGuardHome.yaml
 
 if [ ! -f "$FILE" ]
 then
-   curl -q -s --connect-timeout 1 localhost:3000 > /dev/null && printf 'Waiting for config to be finished' || exit 1
-elif PORT="$(grep '^bind_port:' "$FILE" | cut -f2 -d' ')" && ! curl -q --connect-timeout 1 "localhost:$PORT" > /dev/null 2>&1
+   wget -q --spider --timeout=1 http://localhost:3000 && printf 'Waiting for config to be finished' || exit 1
+elif PORT="$(grep '^bind_port:' "$FILE" | cut -f2 -d' ')" && ! wget -q --spider --timeout=1 "http://localhost:$PORT"
 then
     exit 1
 fi
